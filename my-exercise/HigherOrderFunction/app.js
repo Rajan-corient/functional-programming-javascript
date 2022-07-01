5f7be2d31d5dfab23634237d8d656ea635825994
Rajan Gupta <31067215+Rajan-corient@users.noreply.github.com>
2022-07-01 13:33:28 +0530

    minor textual changes

--- my-exercise/HigherOrderFunction/app.js
+++ my-exercise/HigherOrderFunction/app.js
@@ -6,7 +6,7 @@ list.forEach(function (item, index) {
     console.log(item);
 })
 
-// taking function as ans argument
+// taking function as an argument
 var addNumber = function (num1, getNo) {
     return num1 + getNo();
 };
@@ -15,18 +15,17 @@ console.log(addNumber(5, function(){ return 10 }));
 
 // return function from a function
 // innner function has access to scope of outer function i.e num1 (closure)
-var multipy = function (num1) {
+var multiply = function (num1) {
     return function (num2) {
         return num1 * num2;
     };
 };
 
-let func1 = multipy(5);
+let func1 = multiply(5);
 let func2 = func1(100);
 console.log(func2);
 
 // closure exmaple in module function
-
 var APP = (function module() {
     var a = 3;
 
